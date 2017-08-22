7cc1a67ed44fe25ffd7dcf4686fdeeebc2b77eef
Aditya <aditya.raut@mahiti.org>
2017-08-22 11:52:48 +0530

    updating branch

--- projectmanagement/views.py
+++ projectmanagement/views.py
@@ -10,6 +10,7 @@ from django.contrib.contenttypes.models import ContentType
 # Create your views here.
 
 def create_project(request):
+    # import ipdb; ipdb.set_trace()
     try:
         ids =  request.GET.get('id')
         obj = Project.objects.get(id=ids)
