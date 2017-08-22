10a8cb3e385a2348cbe20f038c2117ee83f07f48
meghana thodaskar <meghana.thodaskar@mahiti.org>
2017-08-22 17:30:30 +0530

    done changes to the project links

--- views/dashboard.py
+++ views/dashboard.py
@@ -8,7 +8,6 @@ def admin_dashboard(request):
     user_id = request.session.get('user_id')
     user_obj = UserProfile.objects.get(user_reference_id = user_id )
     user_id = request.session.get('user_id')
-    user_obj = UserProfile.objects.get(user_reference_id = user_id )
     obj_list = Project.objects.filter(created_by = user_obj)
 #    user = User.objects.get(id = user_obj.id )
     return render(request,'base.html',locals())
