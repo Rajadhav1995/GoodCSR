362518823cc4b3ad8f8feceb84b58cf06294c57f
meghana thodaskar <meghana.thodaskar@mahiti.org>
2017-08-28 17:13:42 +0530

    done the saving and listing of budget line items

--- projectmanagement/templatetags/urs_tags.py
+++ projectmanagement/templatetags/urs_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.auth.models import User
 from projectmanagement.models import (Project, UserProfile)
+from budgetmanagement.models import (Budget,ProjectBudgetPeriodConf,BudgetPeriodUnit)
 
 register = template.Library()
 
@@ -12,4 +13,18 @@ def get_user_project(request):
     project_count = obj_list.count()
     return obj_list
 
+@register.assignment_tag
+def get_budget_lineitem(row):
+    budget_periodobj = ProjectBudgetPeriodConf.objects.filter(row_order = int(row))[0]
+    print row, budget_periodobj
+    try:
+        lineitem = BudgetPeriodUnit.objects.get(budget_period = budget_periodobj)
+    except:
+        lineitem = None
+    return lineitem
 
+@register.assignment_tag
+def get_quarter_details(row,quarter):
+    print quarter
+    line_itemobj = BudgetPeriodUnit.objects.get_or_none(row_order = int(row),quarter_order=int(quarter))
+    return line_itemobj
