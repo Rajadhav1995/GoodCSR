9b76a2ecc2cd3050551a9194c8cda680079c9702
meghana thodaskar <meghana.thodaskar@mahiti.org>
2017-09-01 16:02:09 +0530

    completed the chanegs for budget template design

--- budgetmanagement/manage_budget.py
+++ budgetmanagement/manage_budget.py
@@ -90,8 +90,8 @@ def projectlineitemadd(request):
     projectobj =  Project.objects.get_or_none(slug=project_slug)
     budget_id = request.GET.get('budget_id')
     budgetobj = Budget.objects.get_or_none(id = budget_id )
-    supercategory_list = SuperCategory.objects.filter(active=2,project =projectobj,budget = budgetobj)
-    heading_list = MasterCategory.objects.filter(active=2)
+    supercategory_list = SuperCategory.objects.filter(active=2,project =projectobj,budget = budgetobj).exclude(parent=None)
+    heading_list = MasterCategory.objects.filter(parent__slug="budget-heading",active=2)
     quarter_list = get_budget_quarters(budgetobj)
     if request.method == "POST":
         count = request.POST.get('count')
