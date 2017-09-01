c05854982cc0f38082d549f313bed6dcde948f50
Aditya <aditya.raut@mahiti.org>
2017-09-01 16:54:03 +0530

    page linking, multiselect, ckeditor issue fixed

--- projectmanagement/urls.py
+++ projectmanagement/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from projectmanagement.views import (create_project,project_list,project_detail,project_mapping,
     budget_tranche,key_parameter,tranche_list,add_parameter,
-    upload_parameter,manage_parameter,manage_parameter_values,project_summary,name_valiate)
+    upload_parameter,manage_parameter,manage_parameter_values,project_summary,)
 #from views.dashboard import project_summary
 
 urlpatterns = [
@@ -18,5 +18,5 @@ urlpatterns = [
     url(r'^parameter/manage/$',manage_parameter),
     url(r'^parameter/values/manage/$',manage_parameter_values),
     url(r'^summary/$',project_summary),
-    url(r'^name/validation/$',name_valiate),
+    
 ]
