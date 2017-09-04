644afdeb8ad90cb2b9012a32442e433789605243
meghana thodaskar <meghana.thodaskar@mahiti.org>
2017-09-04 14:46:44 +0530

    done changes to the form design and redirection

--- taskmanagement/views.py
+++ taskmanagement/views.py
@@ -226,10 +226,13 @@ def updates(obj_list):
                         task_uploads={'project_name':project.name,'task_name':task.name,'attach':a.description,
                         'user_name':a.created_by.email,'time':a.created.time(),'date':a.created.date(),'task_status':''}
                     uploads.append(task_uploads)
-    if uploads:
-        uploads = sorted(uploads, key=lambda key: key['date'],reverse=True)
-    else:
-        uploads = []
+    try:
+        if uploads:
+            uploads = sorted(uploads, key=lambda key: key['date'],reverse=True)
+        else:
+            uploads = []
+    except:
+        uploads = uploads
     return uploads
         
 def corp_task_completion_chart(obj_list):
