eee792a414615ff9612962770a5bf80ca0afff21
meghana thodaskar <meghana.thodaskar@mahiti.org>
2017-09-22 14:06:02 +0530

    added the google script

--- menu_decorators.py
+++ menu_decorators.py
@@ -6,10 +6,17 @@ from django.contrib.auth import logout
 from userprofile.models import *
 from django.views.decorators.http import require_POST as post_only
 import urllib
+from views.login import signin
 
 def check_loggedin_access(view):
     def is_auth(request, *args, **kwargs):
         user_id = request.session.get('user_id')
+        next = request.GET.get('next')
+        if request.method == 'POST':
+            user = signin(request)
+        elif user_id == '':
+            message = "Permission Denined!!!Please login with credentials."
+            return render(request, 'login.html', locals())
         return view(request, *args, **kwargs)
     return is_auth
 
