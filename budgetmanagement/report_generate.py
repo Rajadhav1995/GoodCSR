9ba7b9afe36f0dbf06d1cbb3d3d4545671b4a1d6
EswarPriya <eswarpriya.t@mahiti.org>
2017-09-27 17:31:20 +0530

    modified the design and saving the data of report

--- budgetmanagement/report_generate.py
+++ budgetmanagement/report_generate.py
@@ -7,6 +7,7 @@ from media.models import Article,Section,ContactPersonInformation
 from media.forms import ContactPersonForm
 from django.template import loader
 from projectmanagement.models import Project,UserProfile,ProjectFunderRelation
+from budgetmanagement.models import *
 
 
 def report_form(request):
@@ -20,14 +21,22 @@ def report_form(request):
     return render(request,'report/generation-form.html',locals())
 
 def report_detail(request):
-	name = request.POST.get('name')
-	report_name = request.POST.get('report_name')
-	funder = request.POST.get('funder')
-	total_budget = request.POST.get('total_budget')
-	duration = request.POST.get('duration')
-	beneficiaries_no = request.POST.get('beneficiaries_no')
-	implementation_partner = request.POST.get('implementation_partner')
-	description = request.POST.get('description')
-	objective = request.POST.get('objective')
-	
-	return render(request,'project/project_details.html',locals())
+    user_id = request.session.get('user_id')
+    user = UserProfile.objects.get_or_none(user_reference_id = user_id)
+    if request.method == 'POST':
+        data = request.POST
+        project_obj = Project.objects.get_or_none(slug = data.get('project_slug'))
+        project_report = ProjectReport.objects.create(project = project_obj,created_by = user,\
+            report_type = data.get('report_type'),name = project_obj.name)
+        project_report.start_date = data.get('start_date')
+        project_report.end_date = data.get('end_date')
+        project_report.description = data.get('description')
+        project_report.objective = data.get('objective')
+        project_report.save()
+    funder = request.POST.get('funder')
+    total_budget = request.POST.get('total_budget')
+    duration = request.POST.get('duration')
+    beneficiaries_no = request.POST.get('beneficiaries_no')
+    implementation_partner = request.POST.get('implementation_partner')
+    
+    return render(request,'project/project_details.html',locals())
