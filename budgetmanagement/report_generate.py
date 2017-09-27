91751372ec5349b34ddca98410a91aabf0aa1cde
EswarPriya <eswarpriya.t@mahiti.org>
2017-09-27 14:14:35 +0530

    added get or none

--- budgetmanagement/report_generate.py
+++ budgetmanagement/report_generate.py
@@ -10,15 +10,15 @@ from projectmanagement.models import Project,UserProfile,ProjectFunderRelation
 
 
 def report_form(request):
-	slug =  request.GET.get('slug')
-	project = Project.objects.get_or_none(slug = request.GET.get('slug'))
-	funder_user = UserProfile.objects.filter(active=2,organization_type=1)
-	partner = UserProfile.objects.filter(active=2,organization_type=2)
-	mapping_view = ProjectFunderRelation.objects.get(project=project)
-	user_id = request.session.get('user_id')
-	user = UserProfile.objects.get_or_none(user_reference_id = user_id)
-	return render(request,'report/generation-form.html',locals())
+    slug =  request.GET.get('slug')
+    project = Project.objects.get_or_none(slug = request.GET.get('slug'))
+    funder_user = UserProfile.objects.filter(active=2,organization_type=1)
+    partner = UserProfile.objects.filter(active=2,organization_type=2)
+    mapping_view = ProjectFunderRelation.objects.get_or_none(project=project)
+    user_id = request.session.get('user_id')
+    user = UserProfile.objects.get_or_none(user_reference_id = user_id)
+    return render(request,'report/generation-form.html',locals())
 
 def report_detail(request):
-	name = request.POST.get('name')
-	return render(request,'project/project_details.html',locals())
+    name = request.POST.get('name')
+    return render(request,'project/project_details.html',locals())
