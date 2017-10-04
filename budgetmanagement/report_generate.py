6ac09a867b4d88020ad3bb6c3224fc3cc332a13b
Aditya <aditya.raut@mahiti.org>
2017-10-04 17:38:47 +0530

    changes in key parameters

--- budgetmanagement/report_generate.py
+++ budgetmanagement/report_generate.py
@@ -28,23 +28,11 @@ def report_form(request):
 def report_detail(request):
     user_id = request.session.get('user_id')
     user = UserProfile.objects.get_or_none(user_reference_id = user_id)
-    if request.method == 'POST':
-        data = request.POST
-        project_obj = Project.objects.get_or_none(slug = data.get('project_slug'))
-        project_report = ProjectReport.objects.create(project = project_obj,created_by = user,\
-            report_type = data.get('report_type'),name = project_obj.name)
-        project_report.start_date = data.get('start_date')
-        project_report.end_date = data.get('end_date')
-        project_report.description = data.get('description')
-        project_report.objective = data.get('objective')
-        project_report.save()
-    funder = request.POST.get('funder')
-    total_budget = request.POST.get('total_budget')
-    duration = request.POST.get('duration')
-    beneficiaries_no = request.POST.get('beneficiaries_no')
-    implementation_partner = request.POST.get('implementation_partner')
-    
-    return render(request,'project/project_details.html',locals())
+    slug =  request.GET.get('slug')
+    project_obj = Project.objects.get_or_none(slug = slug)
+    funder = ProjectFunderRelation.objects.get(project=project_obj)
+    print funder.funder.name
+    return render(request,'report/report-detail.html',locals())
 
 def get_quarter_report_logic(projectobj):
     ''' common functionality to get the start date,end date and no of quarter'''
