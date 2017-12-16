28770b81bfad0ac2f7b3e579432333d186d3d093
Aditya <aditya.raut@mahiti.org>
2017-12-16 13:01:01 +0530

    changing pdf view page logic

--- budgetmanagement/report_download_pdf.py
+++ budgetmanagement/report_download_pdf.py
@@ -106,9 +106,6 @@ def pdfconverter(request):
     '--header-spacing': '20.10',
     '--encoding': "utf-8",
     '--footer-right': '[page]',
-    '--javascript-delay': '20000',
-    '--no-stop-slow-scripts':'40000',
-    '--debug-javascript':'4000'
     }
     import datetime
     dd = datetime.datetime.today()
