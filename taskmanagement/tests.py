a26f2fce07645f0ba95f9c4989fab4a9c9f6de7b
EswarPriya <eswarpriya.t@mahiti.org>
2018-02-12 16:27:55 +0530

    wrote test case

--- taskmanagement/tests.py
+++ taskmanagement/tests.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
-            'username': 'mahiti1',
+            'username': 'mahiti',
             'password': 'goodcsr@#1234'}
         User.objects.create(**self.credentials)
     def test_login(self):
@@ -15,6 +15,14 @@ class LogInTest(TestCase):
         response = self.client.post('/login/', self.credentials, follow=True)
         # should be logged in now
         self.assertFalse(response.context['user'].is_active)
+        
+    def test_admin(self):
+        user = User.objects.get(username='mahiti')
+        self.assertEqual(user.passsword,'goodcsr@#123')
+        self.assertNotEqual(user.username,'mahiti')
+        
+        
+
 
 
 
