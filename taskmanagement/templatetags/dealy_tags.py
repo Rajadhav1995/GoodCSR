f5c0b7e3d61e2cc30a7b00a2c904f9d9facd70d0
EswarPriya <eswarpriya.t@mahiti.org>
2018-02-12 14:21:23 +0530

    changed the function

--- taskmanagement/templatetags/dealy_tags.py
+++ taskmanagement/templatetags/dealy_tags.py
@@ -16,4 +16,6 @@ from pmu.settings import PMU_URL,SAMITHA_URL
 def super_category_delay(project_slug):
     project_obj = Project.objects.get_or_none(slug=project_slug)
     super_list = SuperCategory.objects.filter(project=project_obj)
+    delay_list =[]
+        
     return super_list
--- taskmanagement/tests.py
+++ taskmanagement/tests.py
@@ -7,14 +7,14 @@ from django.test import TestCase
 class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
-        User.objects.create_user(**self.credentials)
+            'username': 'mahiti1',
+            'password': 'goodcsr@#1234'}
+        User.objects.create(**self.credentials)
     def test_login(self):
         # send login data
         response = self.client.post('/login/', self.credentials, follow=True)
         # should be logged in now
-        self.assertTrue(response.context['user'].is_active)
+        self.assertFalse(response.context['user'].is_active)
 
 
 
