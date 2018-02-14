930361e5c3f8bb5d1868ebe61aeaa04eab0bdbd0
Aditya <aditya.raut@mahiti.org>
2018-02-14 17:37:10 +0530

    removing captcha section for ssl

--- dashboard/project_wall.py
+++ dashboard/project_wall.py
@@ -1,10 +1,14 @@
+import requests,ast
+from django.shortcuts import render
 from taskmanagement.models import Task
 from budgetmanagement.models import Budget
+from projectmanagement.models import Project
 from media.models import Attachment
+
 
 def get_project_updates(request):
 	projobj = Project.objects.get_or_none(slug='test-project-feb5')
 	task_updates = Task.objects.filter(active=2)
 	budget_update = Budget.objects.filter(active=2)
 	file_update = Attachment.objects.filter(active=2)
-
+	return render(request,'project-wall/project_updates.html',locals())
