b575bb5d9b8ff000f301da23a716e53dff65cbe3
EswarPriya <eswarpriya.t@mahiti.org>
2018-02-22 14:44:39 +0530

    commented

--- budgetmanagement/forms.py
+++ budgetmanagement/forms.py
@@ -8,14 +8,41 @@ from django.contrib.contenttypes.models import ContentType
 from userprofile.models import ProjectUserRoleRelationship
 
 # allows you to hit your target and reach your goal. 
-#By setting goals for yourself you are able to measure your progress because you always have a fixed endpoint or benchmark to compare with. Take this scenario for example: David makes a goal to write a book with a minimum of 300 pages. He starts writing every day and works really hard but along the way, he loses track of how many more pages he has written and how much more he needs to write. 
-#So rather than panicking David simply counts the number of pages he has already written and he instantly determines his progress and knows how much further he needs to go.# allows you to hit your target and reach your goal. 
-#By setting goals for yourself you are able to measure your progress because you always have a fixed endpoint or benchmark to compare with. Take this scenario for example: David makes a goal to write a book with a minimum of 300 pages. He starts writing every day and works really hard but along the way, he loses track of how many more pages he has written and how much more he needs to write. 
-#So rather than panicking David simply counts the number of pages he has already written and he instantly determines his progress and knows how much further he needs to go.# allows you to hit your target and reach your goal. 
-#By setting goals for yourself you are able to measure your progress because you always have a fixed endpoint or benchmark to compare with. Take this scenario for example: David makes a goal to write a book with a minimum of 300 pages. He starts writing every day and works really hard but along the way, he loses track of how many more pages he has written and how much more he needs to write. 
-#So rather than panicking David simply counts the number of pages he has already written and he instantly determines his progress and knows how much further he needs to go.# allows you to hit your target and reach your goal. 
-#By setting goals for yourself you are able to measure your progress because you always have a fixed endpoint or benchmark to compare with. Take this scenario for example: David makes a goal to write a book with a minimum of 300 pages. He starts writing every day and works really hard but along the way, he loses track of how many more pages he has written and how much more he needs to write. 
-#So rather than panicking David simply counts the number of pages he has already written and he instantly determines his progress and knows how much further he needs to go.
+#By setting goals for yourself you are able to measure your progress
+# because you always have a fixed endpoint or benchmark to compare with. 
+# Take this scenario for example: David makes a goal to write a book with 
+# a minimum of 300 pages. He starts writing every day and works really hard
+#  but along the way, he loses track of how many more pages he has written and
+#   how much more he needs to write. 
+#So rather than panicking David simply counts the number of pages he has already
+# written and he instantly determines his progress and knows how much further he 
+# needs to go. allows you to hit your target and reach your goal. 
+#By setting goals for yourself you are able to measure your progress because you
+# always have a fixed endpoint or benchmark to compare with. Take this scenario 
+# for example: David makes a goal to write a book with a minimum of 300 pages. 
+# He starts writing every day and works really hard but along the way, he loses 
+# track of how many more pages he has written and how much more he needs to write. 
+#So rather than panicking David simply counts the number of pages he has already
+# written and he instantly determines his progress and knows how much further 
+# he needs to go. allows you to hit your target and reach your goal. 
+#By setting goals for yourself you are able to measure your progress because 
+#you always have a fixed endpoint or benchmark to compare with. Take this 
+#scenario for example: David makes a goal to write a book with a minimum of 
+#300 pages. He starts writing every day and works really hard but along the 
+#way, he loses track of how many more pages he has written and how much more
+# he needs to write. 
+#So rather than panicking David simply counts the number of pages he has 
+#already written and he instantly determines his progress and knows how much 
+#further he needs to go. allows you to hit your target and reach your goal. 
+#By setting goals for yourself you are able to measure your progress because
+# you always have a fixed endpoint or benchmark to compare with. Take this
+#  scenario for example: David makes a goal to write a book with a minimum 
+#  of 300 pages. He starts writing every day and works really hard but along 
+#  the way, he loses track of how many more pages he has written and how much 
+#  more he needs to write. 
+#So rather than panicking David simply counts the number of pages he has 
+#already written and he instantly determines his progress and knows how much 
+#further he needs to go.
 
 def get_tranche_form(slug):
 	project = Project.objects.get_or_none(slug=slug)
