6c04b1e679665cc7aae439d0e8f776121df41592
EswarPriya <eswarpriya.t@mahiti.org>
2018-03-22 12:44:08 +0530

    removed unused function

--- budgetmanagement/report_download_pdf.py
+++ budgetmanagement/report_download_pdf.py
@@ -14,58 +14,6 @@ import StringIO
 import pdfkit
 from pmu.settings import BASE_DIR,PMU_URL
 
-def download_report_generation(request):
-#to download the pdf of the report generated and fetching the details of what to be dislayed in the pdf
-    answer_list ={}
-    answer = ''
-    slug = request.GET.get('slug')
-    report_id = request.GET.get('report_id')
-    project = Project.objects.get_or_none(slug = slug)
-    report_obj = ProjectReport.objects.get_or_none(project=project,id=report_id)
-    mapping_view = ProjectFunderRelation.objects.get_or_none(project=project)
-    report_quarter = QuarterReportSection.objects.filter(project=report_obj)
-    parameter_obj = ProjectParameter.objects.filter(active= 2,project=project,parent=None)
-    cover_image = Attachment.objects.get_or_none(description__iexact = "cover image",attachment_type = 1,
-            content_type = ContentType.objects.get_for_model(report_obj),
-            object_id = report_obj.id)
-    details = {'report_type':report_obj.get_report_type_display(),
-        'report_duration':report_obj.start_date.strftime('%Y-%m-%d')+' TO '+report_obj.end_date.strftime('%Y-%m-%d'),
-        'prepared_by':report_obj.created_by.name,'client_name':mapping_view.funder.organization,
-        'report_name':report_obj.name if report_obj.name else '',
-        'cover_image': cover_image.attachment_file.url if cover_image else '',
-        'project_title':project.name,'project_budget':project.total_budget,
-        'donor':mapping_view.funder.organization,
-        'implement_ngo':mapping_view.implementation_partner.organization,
-        'no_of_beneficiaries':project.no_of_beneficiaries,'project_duration':project.start_date.strftime('%Y-%m-%d')+' TO '+project.end_date.strftime('%Y-%m-%d'),
-        'location':project.get_locations(),'report_quarter':report_quarter}
-        
-    quest_list = Question.objects.filter(active=2,block__block_type = 0).exclude(qtype='OT')
-    for question in quest_list:
-        answer_obj = Answer.objects.get_or_none(question =question,
-                        content_type = ContentType.objects.get_for_model(report_obj),object_id = report_obj.id)
-        if answer_obj and (question.qtype == 'T' or question.qtype == 'APT'):
-            answer = str(answer_obj.text) 
-        elif answer_obj and (question.qtype == 'F' or question.qtype == 'API') and answer_obj.attachment_file:
-            answer = answer_obj.attachment_file.url 
-        else:
-            try:
-                answer = details[str(question.slug)]
-            except:
-                answer = ''
-        answer_list[str(question.slug)] = answer
-    master_pip,master_pin,pin_title_name,pip_title_name,number_json,master_sh = parameter_pie_chart(parameter_obj)
-    # this is to make html to pdf 
-    
-    template = get_template('report/report-pdf-download.html')
-    html = template.render(answer_list)
-    result = StringIO.StringIO()
-    name = project.name.replace(' ','_')
-    file_name = name+'_'+str(report_obj.start_date.strftime('%Y/%m/%d'))+'_To_'+str(report_obj.end_date.strftime('%Y/%m/%d'))+'.pdf'
-    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), dest=result,  link_callback=fetch_resources )
-    response = HttpResponse(result.getvalue(), content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename = '+str(file_name)+''
-    response['pdf'] = pdf
-    return response
     
 def fetch_resources(uri, rel):
     import os
