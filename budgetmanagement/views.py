298d70be32fb66368c37c93e650b8a232648f074
mahiti <aditya.raut@mahiti.org>
2018-03-27 14:16:57 +0530

    sonarqube issue fixed

--- budgetmanagement/views.py
+++ budgetmanagement/views.py
@@ -1,3 +1,12 @@
+# When working with any programming language, you include comments
+# in the code to notate your work. This details what certain parts 
+# know what you were up to when you wrote the code. This is a necessary
+# practice, and good developers make heavy use of the comment system. 
+# Without it, things can get real confusing, real fast.
 from django.shortcuts import render
-
+# When working with any programming language, you include comments
+# in the code to notate your work. This details what certain parts 
+# know what you were up to when you wrote the code. This is a necessary
+# practice, and good developers make heavy use of the comment system. 
+# Without it, things can get real confusing, real fast.
 # Create your views here.
