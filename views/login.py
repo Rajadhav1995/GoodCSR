1cb1e049e5ca4fb1525fae9af9e1848280d17182
EswarPriya <eswarpriya.t@mahiti.org>
2018-06-22 19:20:36 +0530

    changed the json

--- views/login.py
+++ views/login.py
@@ -18,10 +18,10 @@ def signin(request):
         next = request.POST.get('next')
         data = {'username':request.POST.get('username'), 'password':request.POST.get('password')}
         try:
-            r = requests.post(SAMITHA_URL + '/pmu/login/', data=data)
+            res = requests.post(SAMITHA_URL + '/pmu/login/', data=data)
         except requests.exceptions.ConnectionError:
             status_code = "Connection refused"
-        validation_data = json.loads(r.body)
+        validation_data = res.json()
 #        userobj = UserProfile.objects.get_or_none(email=str(request.POST.get('username')))
 #        validation_data = {'status':2,'user_id':int(userobj.user_reference_id) if userobj else ''}
         if validation_data.get('status') == "2":
