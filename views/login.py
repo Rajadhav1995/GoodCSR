fcd7b63c94038c990f4655e705c8c20681129f5b
EswarPriya <eswarpriya.t@mahiti.org>
2018-06-25 19:05:47 +0530

    added simplejson

--- views/login.py
+++ views/login.py
@@ -8,7 +8,9 @@ from pmu.settings import (SAMITHA_URL,)
 from projectmanagement.models import UserProfile
 from media.models import Section,Article
 from django.core.cache import cache
-import json as simplejson
+import urllib3
+import simplejson as json
+
 def signin(request):
     # this function is for
     # login from goodcsr db
@@ -18,10 +20,12 @@ def signin(request):
         next = request.POST.get('next')
         data = {'username':request.POST.get('username'), 'password':request.POST.get('password')}
         try:
-            r = requests.post(SAMITHA_URL + '/pmu/login/', data=data)
+            http = urllib3.PoolManager()
+            r = http.request('POST',SAMITHA_URL + '/pmu/login/', fields = data)
+#            r = requests.post(SAMITHA_URL + '/pmu/login/', data=data)
         except requests.exceptions.ConnectionError:
             status_code = "Connection refused"
-        validation_data = simplejson.loads(r.content)
+        validation_data = json.loads(r.data)
 #        userobj = UserProfile.objects.get_or_none(email=str(request.POST.get('username')))
 #        validation_data = {'status':2,'user_id':int(userobj.user_reference_id) if userobj else ''}
         if validation_data.get('status') == "2":
