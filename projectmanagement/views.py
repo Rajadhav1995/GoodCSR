6ce268a764817a76f10e5f0451430dfb4669c806
mahiti <you@example.com>
2018-11-22 18:06:06 +0530

    added locations for project report csv

--- projectmanagement/views.py
+++ projectmanagement/views.py
@@ -158,13 +158,13 @@ def get_project_report(projects):
     response['Content-Disposition'] = 'attachment; filename="Project_report.csv"'
     writer = csv.writer(response)
     writer.writerow(['Start Date','End Date','Project Name','Managed by','Implementation Partner','Funder','Status','Budget Planned',
-    				'Cause Area','No of Beneficiares','Beneficiary Types'])
+    				'Cause Area','No of Beneficiares','Beneficiary Types','Locations'])
     for pro in projects:
         funder_mapping = get_funder_mapping(pro)
         org_name = get_pmo_user(pro)
         writer.writerow([pro.start_date, pro.end_date, pro.name,org_name,funder_mapping.implementation_partner.organization,
          funder_mapping.funder.organization, pro.get_active_display(), pro.total_budget,pro.get_cause_area(),pro.no_of_beneficiaries,
-		 pro.get_beneficiary()])
+		 pro.get_beneficiary(),pro.get_locations()])
     return response
 
 # get PMO user
